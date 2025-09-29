7ea752faa329dfcc2e732e97da88252af4182909
Ranjan kumar <ranjankumar5673@gmail.com>
2025-09-29 19:39:47 +0530

    version updated

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -57,7 +57,7 @@ const StartScreen = () => {
                             inputMode="none"
                             tabIndex="-1"
                         >
-                            Version 1.94
+                            Version 1.95
                         </button>
                     </div>
           
